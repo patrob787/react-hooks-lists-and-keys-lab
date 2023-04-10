17529f76bad290991e47e9dad741cd4e9ef23879
Patrick Roberts <patrob22@gmail.com>
2023-04-10 17:18:08 -0400

    lab complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  
+  let newLinks = links.map((item) => {
+    return <a href={`#${item}`} key={item}>{item}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{newLinks}</nav>;
 }
 
 export default NavBar;
